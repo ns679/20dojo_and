f770214953eb7998fda9299a6acfbf71e3616028
asagi71 <n.tomo9613@gmail.com>
2020-09-01 13:36:29 +0900

    ViewModelの例を作る

--- app/src/main/java/jp/co/cyberagent/dojo2020/ui/home/HomeViewModel.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2020/ui/home/HomeViewModel.kt
@@ -1,7 +1,35 @@
 package jp.co.cyberagent.dojo2020.ui.home
 
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import jp.co.cyberagent.dojo2020.DI
+import jp.co.cyberagent.dojo2020.data.model.Memo
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel(context: Context) : ViewModel() {
+    private val repository = DI.injectLocalDataSource(context)
 
+    //case A
+    private val mutableLiveData: MutableLiveData<List<Memo>> = MutableLiveData()
+    val liveData: LiveData<List<Memo>>
+        get() = mutableLiveData
+
+    //case B
+    //val liveData = liveData<List<Memo>> {
+    //    emitSource(repository.fetchAll().asLiveData())
+    //}
+
+    fun reload() = viewModelScope.launch {
+        repository.fetchAll().collect {
+            mutableLiveData.value = it
+        }
+    }
+
+    fun save(memo: Memo) = viewModelScope.launch {
+        repository.save(memo)
+    }
 }
--- app/src/main/java/jp/co/cyberagent/dojo2020/ui/main/MainActivity.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2020/ui/main/MainActivity.kt
@@ -1,12 +1,49 @@
 package jp.co.cyberagent.dojo2020.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
-import jp.co.cyberagent.dojo2020.R
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.*
+import jp.co.cyberagent.dojo2020.DI
+import jp.co.cyberagent.dojo2020.data.model.Memo
+import jp.co.cyberagent.dojo2020.databinding.ActivityMainBinding
+import jp.co.cyberagent.dojo2020.ui.home.HomeViewModel
+import jp.co.cyberagent.dojo2020.ui.home.HomeViewModelFactory
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val viewModel: HomeViewModel by viewModels {
+        HomeViewModelFactory(this, Bundle(), this)
+    }
+
+    @ExperimentalStdlibApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            viewModel.liveData.observe(this@MainActivity) {
+                val memo = it.randomOrNull() ?: return@observe
+
+                helloWorld.text = memo.title
+            }
+
+            reloadButton.setOnClickListener { viewModel.reload() }
+            saveButton.setOnClickListener {
+                val title = titleEditText.text.toString()
+
+                viewModel.save(createWithTitle(title))
+            }
+        }
+
+    }
+
+    private fun createWithTitle(title: String): Memo {
+        return Memo(title, "contents", 0.256)
     }
 }
